66c3c473eb7cd8725e7c65cf880f8d06fff766aa
lichfieldcodeclub@gmail.com <lichfieldcodeclub@gmail.com>
2021-12-04 09:36:22 +0000

    Change degrees to radians

--- lift.py
+++ lift.py
@@ -3,9 +3,10 @@ import math
 v = 10 # metres per second
 p = 1.225 # air density (rho) kg per cubic metre
 a = 10 # attack angle (alpha) degrees
+r = math.radians(a)
 d = 0 # drag
 q = 0.5 * ( p * (v**2))
-c = 2 * math.pi * a # lift coefficient
+c = 2 * math.pi * r # lift coefficient
 l = q * c # lift per unit chord
 
 print(f"The lift is {l:.3f}N")
