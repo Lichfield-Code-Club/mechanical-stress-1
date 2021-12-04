8d1e34a0e277293c8f87b2257c105787fd024587
Mark Pritchett <unixmiddleware@gmail.com>
2021-12-04 08:05:23 +0000

    Refactored by assigning variables to partial evaluations

--- inertia.py
+++ inertia.py
@@ -14,6 +14,13 @@ oscillations = [11,12,11,12,9] # time for 10 swings, seconds
 total_duration = sum(oscillations)
 num_oscillations = len(oscillations) * 10
 t = total_duration/num_oscillations
-I = (((t / (2*math.pi)**2)*(m*g*d)- (m*(d**2)))) # moment of inertia, kg per square metre
 
-print(f'I = {i:.3e} kg/m\N{SUPERSCRIPT TWO}') # https://unicode.org/Public/UNIDATA/NamesList.txt
+twopi = math.pi * 2
+tsq = (t / twopi) ** 2
+mgd = m * g * d
+dsq = d ** 2
+mdsq = m * dsq
+
+I = (tsq * mgd) - mdsq # moment of inertia, kg per square metre
+
+print(f'I = {I:.3e} kg/m\N{SUPERSCRIPT TWO}') # https://unicode.org/Public/UNIDATA/NamesList.txt
